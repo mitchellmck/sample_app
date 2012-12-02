5048590917d494a3dd7957d9c6b5f08c3120a197
Mitchell McKenzie <mitch_757@mac.com>
2012-12-02 18:24:44 +0000

    add sign in and sign out functionality

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -110,5 +110,14 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+  
+    it {should respond_to(:password_confirmation)}
+    it {should respond_to(:remember_token)}
+    it {should respond_to(:authenticate)}
+
+    describe "remember token" do 
+      before { @user.save}
+      its(:remember_token) {should_not be_blank}
+    end
   end
 end
